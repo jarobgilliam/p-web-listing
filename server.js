14980c1a5726ed0cf9fb046c178727597f8bb3e1
Jarob Gilliam <jarobgilliambiz@gmail.com>
2017-03-29 21:23:17 -0500

    path issue

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('client'));
+app.use(express.static('/client/img'));
 
 app.listen(3000, function() {
   console.log('Listening on 3000...');
