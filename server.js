a028b128c9a8b7716ab3f62f664a47b61c4aa96e
Jarob Gilliam <jarobgilliambiz@gmail.com>
2017-03-29 21:11:00 -0500

    fix paths

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('client'));
+app.use('/client', express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function() {
   console.log('Listening on 3000...');
