d4eb898df97e82de63662c59c5368d3100c85a6c
Jarob Gilliam <jarobgilliambiz@gmail.com>
2017-03-29 21:13:07 -0500

    paths...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/client', express.static(path.join(__dirname, 'public')));
+app.use(express.static('client'));
 
 app.listen(3000, function() {
   console.log('Listening on 3000...');
