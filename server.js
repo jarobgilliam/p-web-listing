82d02dc3dfa197aabf0f5a6aa155fc40404b348b
Jarob Gilliam <jarobgilliambiz@gmail.com>
2017-03-29 21:27:59 -0500

    path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('client'));
-app.use(express.static('/client/img'));
+app.use(express.static(__dirname + '/client'));
+
 
 app.listen(3000, function() {
   console.log('Listening on 3000...');
